fdc086ffeb1d253cb08dcca54657d13a7ca41f45
Oleksandr S <oleksandr.slv@gmail.com>
2021-12-10 22:19:41 +0200

    netlify deployed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import Container from './components/Container/Container';
 import AppBar from './components/AppBar/AppBar';
-import HomeView from './views/HomeView';
-import MoviesView from './views/MoviesView';
-import MovieDetailsView from './views/MovieDetailsView';
+import Loader from 'react-loader-spinner';
 import { Routes, Route } from 'react-router';
+import { lazy, Suspense } from 'react';
+
+const HomeView = lazy(() => import('./views/HomeView'));
+const MoviesView = lazy(() => import('./views/MoviesView'));
+const MovieDetailsView = lazy(() => import('./views/MovieDetailsView'));
 
 function App() {
   return (
     <Container>
       <AppBar />
 
-      <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route path="/movies" element={<MoviesView />} />
-        <Route path="/movies/:movieId/*" element={<MovieDetailsView />} />
-      </Routes>
+      <Suspense fallback={<Loader type="TailSpin" color="#00BFFF" height={80} width={80} className="Loader" />}>
+        <Routes>
+          <Route path="/" element={<HomeView />} />
+          <Route path="/movies" element={<MoviesView />} />
+          <Route path="/movies/:movieId/*" element={<MovieDetailsView />} />
+        </Routes>
+      </Suspense>
     </Container>
   );
 }
